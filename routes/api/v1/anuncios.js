86832353de159579200475838b43cfac8c694c5e
Adrian Perez Lopez <a.przlopez@gmail.com>
2016-08-20 14:07:14 +0200

    Filtros de anuncios terminado.

--- routes/api/v1/anuncios.js
+++ routes/api/v1/anuncios.js
@@ -11,9 +11,9 @@ router.get('/', function (req, res, next) {
    
     var nombre = req.query.nombre;
     var venta = req.query.venta;
-    var precio = req.query.precio;
+    var precio = req.query.precio || null;
     var foto = req.query.foto;
-    var tags = req.query.tags;
+    var tags = req.query.tags || null;
 
     //Para indicar por que fila empiezo, si no indico ninguna, por defecto empiezo por la 0.
     var start = parseInt(req.query.start) || 0;
@@ -24,16 +24,76 @@ router.get('/', function (req, res, next) {
     //Orden 1 ó -1, por defecto NULL.
     var sort = req.query.sort || null;
 
-    //Inicializo filters.
-    var filters = {};
+    //Inicializo criteria (filtros).
+    var criteria = {};
 
     if(typeof nombre !== 'undefined'){
 
-        filters.nombre = nombre;
+        //Búsqueda por expresión regular.
+        criteria.nombre = new RegExp('^' + req.query.nombre, 'i');
 
     }
 
-    Anuncio.list(filters, start, limit, sort, function (err, rows) {
+    if(typeof venta !== 'undefined'){
+
+        //Venta, string.
+        criteria.venta = req.query.venta;
+
+    }
+
+    if(typeof precio !== null){
+
+        //50 buscará los que tengan precio igual a 50  {precio:'50'}
+        //Si no lo encuentra, devuelve -1.
+        if(precio.indexOf('-') !== -1) {
+
+            //Separo si me viene con guión (10­50), ojo split me transforma los Number a String.
+            var precio_split = precio.split('-');
+            var precio_iz = parseInt(precio_split[0]) || 0;
+            var precio_der = parseInt(precio_split[1]) || 0;
+
+            //­50 buscará los que tengan precio menor de 50  {precio:{'$lte': '50' } }
+            if (precio_iz === 0) {
+
+                criteria.precio = {$lte: precio_der};
+
+                //10­ buscará los que tengan precio mayor que 10  {precio:{'$gte': '10' } }
+            } else if (precio_der === 0) {
+
+                criteria.precio = {$gte: precio_iz};
+
+                //10­50 buscará anuncios con precio incluido entre estos valores  {precio: { '$gte': '10', '$lte': '50' } }
+            } else {
+
+                criteria.precio = {$gte: precio_iz, $lte: precio_der};
+
+            }
+
+        }else{
+
+            criteria.precio = req.query.precio;
+
+        }
+
+    }
+
+    if(tags !== null){
+
+        //https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Array/isArray
+        if (Array.isArray(tags)) {
+
+            filters.tag = {$in: tags};
+
+        } else {
+
+            //Tan sólo un elemento.
+            filters.tag = tags;
+
+        }
+
+    }
+
+    Anuncio.list(criteria, start, limit, sort, function (err, rows) {
 
         if(err){
 
